3b2aaf023ebf642d24036f6b384bb19f1a68cded
Maikon Oliveira Campos <141969456+maiklongline@users.noreply.github.com>
2024-10-09 20:38:07 -0300

    Add files via upload

---
+++ buscaFilme.js
@@ -0,0 +1,10 @@
+const chave_Api = 'd5ee36e5';
+
+const formulario = document.getElementById('frmBuscaFilme');
+const nomeBusca = document.getElementById('nameFilme');
+const resultadoDiv = document.getElementById('resultado');
+
+function buscaFilme() {
+    
+    resultadoDiv.innerHTML = '<p>Carregando...</p>'
+}
