c45e7fa6d7f409fc65c00619af5bbc36ea3e586b
Maikon Oliveira Campos <141969456+maiklongline@users.noreply.github.com>
2024-10-09 22:36:10 -0300

    Add files via upload

--- buscaFilme.js
+++ buscaFilme.js
@@ -1,10 +1,29 @@
-const chave_Api = 'd5ee36e5';
 
-const formulario = document.getElementById('frmBuscaFilme');
-const nomeBusca = document.getElementById('nameFilme');
-const resultadoDiv = document.getElementById('resultado');
 
-function buscaFilme() {
+function buscaFilmes() {
+    const chave_API = 'd5ee36e5';
+
+    // const formulario = document.getElementById('frmBuscarFilmes');
+    const nomeBusca = document.getElementById('nameFilme');
+    const resultadoDiv = document.getElementById('resultado');
     
-    resultadoDiv.innerHTML = '<p>Carregando...</p>'
+    resultadoDiv.innerHTML = '<p>Carregar...<p>';
+
+
+    const url = `http://www.omdbapi.com/?apikey=${chave_API}&s=${encodeURIComponent(nomeBusca.value)}`
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            resultadoDiv.innerHTML = '';
+
+            data.Search.forEach(filme => {
+                resultadoDiv.innerHTML +=
+                    `<h2>${filme.Title} (${filme.Year})</h2>
+                    <img src="${filme.Poster} width="200">`;
+                    
+                
+            });
+        });
+
 }
